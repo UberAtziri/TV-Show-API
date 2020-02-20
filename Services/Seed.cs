31c29974507a8481f17ae673835a608c91ebaa1f
UberAtziri <kolyada.aleksey@gmail.com>
2020-02-20 15:40:52 +0200

    casual commit

--- Services/Seed.cs
+++ Services/Seed.cs
@@ -13,9 +13,6 @@ namespace WebApi.Services
          context.TVShowItems.Add(new TVShowEntity(){Title = "The Haunting of Hill House", Genre = "Horror", ImdbScore = 8.7 });   
          context.TVShowItems.Add(new TVShowEntity(){Title = "Sex Education", Genre = "Comedy", ImdbScore = 8.3 });   
          context.TVShowItems.Add(new TVShowEntity(){Title = "Dark", Genre = "Mystery", ImdbScore = 8.7 });  
-         context.TVShowItems.Add(new TVShowEntity(){Title = "Random1", Genre = "Drama", ImdbScore = 9.0 });   
-         context.TVShowItems.Add(new TVShowEntity(){Title = "Random2", Genre = "Drama", ImdbScore = 9.0 });   
-         context.TVShowItems.Add(new TVShowEntity(){Title = "Random3", Genre = "Drama", ImdbScore = 9.0 });   
 
          await context.SaveChangesAsync(); 
         }
